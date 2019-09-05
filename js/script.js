7110a7679c8294067580ec14141011bf2b9d9aa0
dmytroony <dm.onysko@gmail.com>
2019-09-05 11:59:10 +0300

    template of an average price

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let comboA = 6.75;
+let comboB = 7.5;
+let comboC = 5.75;
+let comboD = 8;
+
+let sumCombo = comboA + comboB + comboC + comboD;
+let amountOfCombo = 4;
+let averagePrice = sumCombo / amountOfCombo;
+
+console.log(averagePrice);
