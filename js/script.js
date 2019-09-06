915c435e79eee16a028beab758ff182d276aa429
dmytroony <dm.onysko@gmail.com>
2019-09-06 17:26:19 +0300

    finished chapter 2; done a homework

--- js/script.js
+++ js/script.js
@@ -1,10 +1,59 @@
-let comboA = 6.75;
-let comboB = 7.5;
-let comboC = 5.75;
-let comboD = 8;
+// Chapter 1:
+// let comboA = 6.75;
+// let comboB = 7.5;
+// let comboC = 5.75;
+// let comboD = 8;
+//
+// let sumCombo = comboA + comboB + comboC + comboD;
+// //let amountOfCombo = 4;
+// // let averagePrice = sumCombo / amountOfCombo;
+//
+// let comboE = 8.25;
+// sumCombo = sumCombo + comboE;
+// amountOfCombo = 5;
+// averagePrice = sumCombo /amountOfCombo;
+//
+// console.log(averagePrice);
 
-let sumCombo = comboA + comboB + comboC + comboD;
-let amountOfCombo = 4;
-let averagePrice = sumCombo / amountOfCombo;
 
-console.log(averagePrice);
+// //  Chapter 2
+//
+// // Example 1:
+// let favoriteSupplement = 'pepperoni';
+// let secondFavoriteSupplement = 'sausages';
+// let classyPizza = favoriteSupplement + ' and ' + secondFavoriteSupplement;
+// console.log(classyPizza);
+//
+// // Example 2:
+// var total = 6 + 7 + 8;
+// console.log(total);
+//
+// // Example 3:
+// console.log((8 * 3) / 6);
+//
+// // Example 4:
+// var ageBrother = 11;
+// var ageSister = 13;
+// var amountOfBrothersOrSisters = 2;
+// var averageAge = (ageBrother + ageSister) / amountOfBrothersOrSisters;
+// console.log(averageAge);
+//
+// // Example 5:
+// let coolNumber = 5;
+// let coolAnswer = (20 / coolNumber) + ( (8 * coolNumber) / (6-2) );
+// console.log(coolAnswer);
+//
+// // Example 6:
+// console.log( 6 + ' feet' + 7 + ' feet' );
+//
+// // Example 7:
+// console.log('Working from ' + 9 + ' to' + 5);
+//
+// // Example 8:
+// console.log(7 + '3');
+//
+// // Example 9:
+// console.log(12 / 0);
+
+// Chapter 3:
+
