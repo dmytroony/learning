72792eca8b4f11de3cb69a389d82d09c888f17b3
dmytroony <dm.onysko@gmail.com>
2019-09-26 22:30:55 +0300

    Chpater 1,2 finished.

---
+++ js/scriptfile.js
@@ -0,0 +1,12 @@
+// function openWindow() {
+//
+// 	let msgWindow = open('../index2.html');
+//
+// }
+
+// if (window.confirm('Push the Ok or the Cancel')) {
+// 	window.alert('Ok!');
+// } else {
+// 	window.alert('Cancel!');
+// }
+
