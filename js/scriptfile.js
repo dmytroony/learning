a7196a39682e9aa91b2daa099354a502df38db7d
dmytroony <dm.onysko@gmail.com>
2019-09-30 18:13:36 +0300

    Chapter 3 finished.

--- js/scriptfile.js
+++ js/scriptfile.js
@@ -10,3 +10,64 @@
 // 	window.alert('Cancel!');
 // }
 
+// console.log(1+'1');
+
+// let a = 0;
+// let b = 1;
+// let c;
+//
+// if (a !== 0) {
+// 	c = 10 / a;
+// 	document.write('c = ' + c);
+// } else if (b !== 0) {
+// 	c = 10 / b;
+// 	document.write('c = ' + c);
+// } else {
+// 	document.write('Divide by 0');
+// }
+
+// let command = window.prompt('Enter the action.', '');
+//
+// if(command === null) {
+// 	console.log('Cancel pressed!');
+// } else {
+// 	switch ( parseInt(command) ) {
+// 		case 1: console.log('Action 1 selected'); break;
+// 		case 2: console.log('Action 2 selected'); break;
+// 		case 3: console.log('Action 3 selected'); break;
+// 		case 4: console.log('Action 4 selected'); break;
+// 		default: console.log('Unknown action!');
+// 	}
+// }
+
+// let i = 1;
+//
+// while (i < 11) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// do {
+// 	console.log(i);
+// 	i++;
+// } while (i <= 10);
+
+// for (let i = 1; i < 11; i++) {
+// 	if (i === 5) break; {
+// 		console.log(i + ' ');
+// 	}
+// }
+
+// let  j = 1;
+//
+// while (j < 15) {
+//
+// 	for (let k = 0; k < j; k++)
+//
+// 		document.write('*');
+// 	document.write('<br>');
+//
+// 	j++;
+//
+// }
+
