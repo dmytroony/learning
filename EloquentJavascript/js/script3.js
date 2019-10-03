dbfb6fea37cedf0e70b032d3ab1bc89c7a5744ea
dmytroony <dm.onysko@gmail.com>
2019-10-03 23:52:48 +0300

    Chapter 2 update

--- EloquentJavascript/js/script3.js
+++ EloquentJavascript/js/script3.js
@@ -3,5 +3,53 @@
 //Chapter 2:
 
 // let theNumber = Number(prompt('Enter the number'));
-// console.log('This number is the square root of ' + (theNumber * theNumber));
+// // console.log('This number is the square root of ' + (theNumber * theNumber));
+//
+// if (!Number.isNaN(theNumber)) {
+// 	console.log('This number is the square root of ' + (theNumber * theNumber));
+// } else {
+// 	console.log('Why did you enter not a number?!?');
+// }
+
+// let num = Number(prompt('Enter the number!'));
+//
+// if (num < 10) {
+// 	alert('Low');
+// } else if (num < 100) {
+// 	alert('Medium');
+// } else {
+// 	alert('High');
+// }
+
+// let num = 0;
+//
+// while (num <= 12) {
+//
+// 	console.log(num);
+// 	num += 2;
+//
+// }
+
+// let result = 1;
+// let counter = 0;
+//
+// while (counter < 10) {
+//
+// 	result *= 2;
+// 	counter++;
+//
+// 	console.log(counter + ') ' + result);
+// }
+//
+// console.log(result);
+
+// let yourName;
+//
+// do {
+//
+// 	yourName = prompt('Who are you?', '');
+//
+// } while (!yourName);
+//
+// console.log(yourName);
 
