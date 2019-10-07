f2b7486372d48e6a7349689baef05bd97099ea9f
dmytroony <dm.onysko@gmail.com>
2019-10-07 22:05:11 +0300

    Chapter 3 (Functions). + Update

--- EloquentJavascript/js/script3.js
+++ EloquentJavascript/js/script3.js
@@ -148,3 +148,100 @@
 //
 // console.log(row);
 
+// Chapter 3:
+
+// const square = function (x) {
+// 	return (x * x);
+// };
+//
+// console.log(square(12));
+
+// const makeNoise = function () {
+// 	console.log('YEAH-H-H-H!!!');
+// };
+//
+// makeNoise();
+
+// const power = function (base, exponent) {
+//
+// 	let result = 1;
+//
+// 	for (let count = 0; count < exponent; count++) {
+//
+// 		result *= base;
+//
+// 	}
+//
+// 	return result;
+//
+// };
+//
+// console.log(power(2, 10));
+
+// let x = 10;
+//
+// if (x === false) {
+// 	let y = 20;
+// 	var z = 30;
+// 	console.log(x + y + z);
+// }
+//
+// console.log(x + z);
+
+// const halve = function (n) {
+// 	return n / 2;
+// };
+//
+// let n = 10;
+// console.log(halve(100));
+// console.log(n);
+
+// const humus = function (factor) {
+//
+// 	const ingredient = function (amount, unit, name) {
+//
+// 		let ingredientAmount = amount * factor;
+// 		if (ingredientAmount > 1) {
+// 			unit += 's';
+// 		}
+// 		console.log(`${ingredientAmount} ${unit} ${name}`);
+//
+// 	};
+//
+// 	ingredient(1, 'bank', 'chickpeas');
+// 	ingredient(0.25, 'glass', 'sesame paste');
+// 	ingredient(0.25, 'glass', 'lemon juice');
+// 	ingredient(1, 'teeth', 'garlic');
+// 	ingredient(2, 'tablespoon', 'olive oil');
+// 	ingredient(0.5, 'teaspoon', 'cumin');
+//
+// };
+// humus(1);
+
+// Arrow functions
+
+// let launchMissiles = function () {
+// 	missileSystem.launch('now');
+// };
+// if (safeMode) {
+// 	launchMissiles() = function () {/* do it nothing */}
+// }
+
+// const power = (base, exponent) => {
+// 	let result = 1;
+// 	for (let count = 0; count < exponent; count++) {
+// 		result *= base;
+// 	}
+// 	return result;
+// };
+// console.log(power(3, 3));
+
+// const square1 = (x) => {return x * x};
+// const square2 = x => x * x;
+// // square1 === square2;
+
+// const horn = () => {
+// 	console.log('Toot');
+// };
+// horn();
+
