a0112be060f8ed0ae38618a5e486305fa101804e
dmytroony <dm.onysko@gmail.com>
2019-10-15 23:20:54 +0300

    Chapter 4 Update

--- EloquentJavascript/js/script3.js
+++ EloquentJavascript/js/script3.js
@@ -822,11 +822,15 @@
 // 	true);
 
 // function phi(table) {
-// 	return (table[3] + table[0] - table[2] * table[1]) /
-// 	Math.sqrt((table[2] + table[3]) *
-// 		(table[0] + table[1]) *
-// 		(table[1] + table[3]) *
-// 		(table[0] + table[2]));
+//
+// 	return ( table[3] + table[0] - table[2] * table[1]) /
+// 		Math.sqrt(
+// 		 (table[2] + table[3]) *
+// 			(table[0] + table[1]) *
+// 			(table[1] + table[3]) *
+// 			(table[0] + table[2])
+// 		);
+//
 // }
 // console.log(phi([76, 9, 4, 1]));
 
@@ -1039,3 +1043,4 @@ function tableFor(event, journal) {
 
 }
 console.log(tableFor('pizza', journal));
+
