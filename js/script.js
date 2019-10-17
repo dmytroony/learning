0fde1586d8191b7ed862761eaa932032f1b59f59
dmytroony <dm.onysko@gmail.com>
2019-10-17 16:06:50 +0300

    Learning Update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log(this);
+
+function abc() {
+	console.log(this);
+	console.log('Hello world!');
+	this.style.background = 'red';
+}
+
+// document.querySelector('.lorem').onclick = abc;
+
+let p = document.querySelectorAll('p');
+
+// p.forEach(function (element) {
+// 	element.onclick = abc;
+// });
+// console.log(this);
+
+// document.querySelector('.lorem')
+// 	.addEventListener('click', abc);
+
+p.forEach(function (element) {
+	element.addEventListener('click', abc);
+});
