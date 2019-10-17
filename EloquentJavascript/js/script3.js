ccfb93cb88a6006ca7a77cf589acb66e7a55db58
dmytroony <dm.onysko@gmail.com>
2019-10-17 16:07:26 +0300

    Chapter 4 Update

--- EloquentJavascript/js/script3.js
+++ EloquentJavascript/js/script3.js
@@ -821,254 +821,321 @@
 // 		'break', 'peanut', 'beer'],
 // 	true);
 
-function phi(table) {
+// function phi(table) {
+//
+// 	return ( table[3] + table[0] - table[2] * table[1]) /
+// 		Math.sqrt(
+// 		 (table[2] + table[3]) *
+// 			(table[0] + table[1]) *
+// 			(table[1] + table[3]) *
+// 			(table[0] + table[2])
+// 		);
+//
+// }
+// console.log(phi([76, 9, 4, 1]));
+//
+// let journal = [
+// 	{"events":["carrot","exercise","weekend"],
+// 		"squirrel":false},
+// 	{"events":["bread","pudding","brushed teeth","weekend","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["carrot","nachos","brushed teeth","cycling","weekend"],
+// 		"squirrel":false},
+// 	{"events":["brussel sprouts","ice cream","brushed teeth","computer","weekend"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","candy","brushed teeth","exercise","weekend","dentist"],
+// 		"squirrel":false},
+// 	{"events":["brussel sprouts","pudding","brushed teeth","running","weekend"],
+// 		"squirrel":false},
+// 	{"events":["pizza","brushed teeth","computer","work","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["bread","beer","brushed teeth","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","brushed teeth","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","nachos","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","weekend","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","brushed teeth","television","weekend"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","computer","work"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","nachos","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","reading","weekend"],
+// 		"squirrel":false},
+// 	{"events":["bread","brushed teeth","weekend"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","brushed teeth","exercise","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","brushed teeth","reading","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","ice cream","brushed teeth","television","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","nachos","work"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","ice cream","brushed teeth","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","peanuts","computer","weekend"],
+// 		"squirrel":true},
+// 	{"events":["potatoes","ice cream","brushed teeth","computer","weekend"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","ice cream","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["peanuts","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","exercise","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","ice cream","computer","work"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","ice cream","work"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","candy","reading","weekend"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","nachos","brushed teeth","running","weekend"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","beer","work","dentist"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","pudding","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","brushed teeth","reading","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","pudding","television","weekend"],
+// 		"squirrel":false},
+// 	{"events":["bread","brushed teeth","exercise","weekend"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","peanuts","work"],
+// 		"squirrel":true},
+// 	{"events":["pizza","work"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","exercise","work"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","exercise","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","brushed teeth","television","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","cycling","weekend"],
+// 		"squirrel":false},
+// 	{"events":["carrot","brushed teeth","weekend"],
+// 		"squirrel":false},
+// 	{"events":["carrot","beer","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","peanuts","candy","work"],
+// 		"squirrel":true},
+// 	{"events":["carrot","peanuts","brushed teeth","reading","work"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","peanuts","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","nachos","brushed teeth","exercise","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","peanuts","brushed teeth","television","weekend"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","brushed teeth","cycling","weekend"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","peanuts","brushed teeth","computer","work","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","brushed teeth","television","work"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","brushed teeth","computer","work"],
+// 		"squirrel":false},
+// 	{"events":["bread","candy","work"],
+// 		"squirrel":false},
+// 	{"events":["potatoes","nachos","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","pudding","brushed teeth","weekend"],
+// 		"squirrel":false},
+// 	{"events":["carrot","brushed teeth","exercise","weekend","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["brussel sprouts","running","work"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["candy","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["brussel sprouts","brushed teeth","computer","work"],
+// 		"squirrel":false},
+// 	{"events":["bread","brushed teeth","weekend"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","brushed teeth","weekend"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","candy","television","work","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["carrot","pudding","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","ice cream","brushed teeth","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["spaghetti","peanuts","exercise","weekend"],
+// 		"squirrel":true},
+// 	{"events":["bread","beer","computer","weekend","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","peanuts","brushed teeth","work","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","brushed teeth","television","work"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["carrot","reading","weekend"],
+// 		"squirrel":false},
+// 	{"events":["carrot","peanuts","reading","weekend"],
+// 		"squirrel":true},
+// 	{"events":["potatoes","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["lasagna","ice cream","work","touched tree"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","peanuts","brushed teeth","cycling","work"],
+// 		"squirrel":false},
+// 	{"events":["pizza","brushed teeth","running","work"],
+// 		"squirrel":false},
+// 	{"events":["lettuce","brushed teeth","work"],
+// 		"squirrel":false},
+// 	{"events":["bread","brushed teeth","television","weekend"],
+// 		"squirrel":false},
+// 	{"events":["cauliflower","peanuts","brushed teeth","weekend"],
+// 		"squirrel":false}
+// ];
+//
+// function hasEvent(event, entry) {
+//
+// 	return entry.events.indexOf(event) !== -1;
+//
+// }
+//
+// function tableFor(event, journal) {
+//
+// 	let table;
+// 	table = [0, 0, 0, 0];
+//
+// 	for (let i = 0; i < journal.length; i++) {
+//
+// 		let entry = journal[i], index = 0;
+//
+// 		if (hasEvent(event, entry)) index +=1;
+// 		if (entry.squirrel) index += 2;
+//
+// 		table[index] += 1;
+//
+// 	}
+//
+// 	return table;
+//
+// }
+// console.log(tableFor('pizza', journal));
+//
+// let map = {};
+// function storePhi(event, phi) {
+// 	map[event] = phi;
+// }
+//
+// storePhi('pizza', 0.069);
+// storePhi('touched a tree', -0.081);
+// console.log('pizza' in map);
+// console.log(map['touched a tree']);
+//
+// for (let event in map)
+// console.log("Correlation for '" + event + "' = " + map[event]);
+//
+// function gatherCorrelations(journal) {
+// 	let phis = {};
+// 	for (let entry = 0; entry < journal.length; entry++) {
+// 		let events = journal[entry].events;
+// 		for (let i = 0; i < events.length; i++) {
+// 			let event = events[i];
+// 			if (!(event in phis))
+// 				phis[event] = phi(tableFor(event, journal));
+// 		}
+// 	}
+// 	return phis;
+// }
+//
+// let correlations = gatherCorrelations(journal);
+// console.log(correlations.pizza);
 
-	return ( table[3] + table[0] - table[2] * table[1]) /
-		Math.sqrt(
-		 (table[2] + table[3]) *
-			(table[0] + table[1]) *
-			(table[1] + table[3]) *
-			(table[0] + table[2])
-		);
+// This
 
+// function test() {
+// 	return this;
+// }
+// test();
+
+// function abc() {
+// 	console.log(this);
+// 	console.log('Hello world!');
+// 	this.style.background = 'red';
+// }
+
+// // document.querySelector('.lorem').onclick = abc;
+
+// let p = document.querySelectorAll('p');
+
+// // p.forEach(function (element) {
+// // 	element.onclick = abc;
+// // });
+// // console.log(this);
+
+// // document.querySelector('.lorem')
+// // 	.addEventListener('click', abc);
+
+// p.forEach(function (element) {
+// 	element.addEventListener('click', abc);
+// });
+
+// console.log(this);
+
+// let test = {
+//     prop: 42,
+//     func: function () {
+//         return this.prop;
+//     },
+// };
+// console.log(test.func());
+
+// console.log(this.document === document); // true
+// console.log(this === window); // true
+// console.log(this.document !== window); // true
+
+// this.a = 37;
+// console.log(window.a); //37
+
+// function f1() {
+//     return this;
+// }
+// console.log(f1() === undefined);
+
+// function f2() {
+//     'use strict';
+//     return this;
+// }
+// console.log(f2() === undefined);
+
+let obj = {
+    a: 'Custom'
+};
+let a = 'Global';
+
+function whatsThis() {
+    return this.a;
 }
-console.log(phi([76, 9, 4, 1]));
-
-let journal = [
-	{"events":["carrot","exercise","weekend"],
-		"squirrel":false},
-	{"events":["bread","pudding","brushed teeth","weekend","touched tree"],
-		"squirrel":false},
-	{"events":["carrot","nachos","brushed teeth","cycling","weekend"],
-		"squirrel":false},
-	{"events":["brussel sprouts","ice cream","brushed teeth","computer","weekend"],
-		"squirrel":false},
-	{"events":["potatoes","candy","brushed teeth","exercise","weekend","dentist"],
-		"squirrel":false},
-	{"events":["brussel sprouts","pudding","brushed teeth","running","weekend"],
-		"squirrel":false},
-	{"events":["pizza","brushed teeth","computer","work","touched tree"],
-		"squirrel":false},
-	{"events":["bread","beer","brushed teeth","cycling","work"],
-		"squirrel":false},
-	{"events":["cauliflower","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["pizza","brushed teeth","cycling","work"],
-		"squirrel":false},
-	{"events":["lasagna","nachos","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["brushed teeth","weekend","touched tree"],
-		"squirrel":false},
-	{"events":["lettuce","brushed teeth","television","weekend"],
-		"squirrel":false},
-	{"events":["spaghetti","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["brushed teeth","computer","work"],
-		"squirrel":false},
-	{"events":["lettuce","nachos","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["carrot","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["brushed teeth","work"],
-		"squirrel":false},
-	{"events":["cauliflower","reading","weekend"],
-		"squirrel":false},
-	{"events":["bread","brushed teeth","weekend"],
-		"squirrel":false},
-	{"events":["lasagna","brushed teeth","exercise","work"],
-		"squirrel":false},
-	{"events":["spaghetti","brushed teeth","reading","work"],
-		"squirrel":false},
-	{"events":["carrot","ice cream","brushed teeth","television","work"],
-		"squirrel":false},
-	{"events":["spaghetti","nachos","work"],
-		"squirrel":false},
-	{"events":["cauliflower","ice cream","brushed teeth","cycling","work"],
-		"squirrel":false},
-	{"events":["spaghetti","peanuts","computer","weekend"],
-		"squirrel":true},
-	{"events":["potatoes","ice cream","brushed teeth","computer","weekend"],
-		"squirrel":false},
-	{"events":["potatoes","ice cream","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["peanuts","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["potatoes","exercise","work"],
-		"squirrel":false},
-	{"events":["pizza","ice cream","computer","work"],
-		"squirrel":false},
-	{"events":["lasagna","ice cream","work"],
-		"squirrel":false},
-	{"events":["cauliflower","candy","reading","weekend"],
-		"squirrel":false},
-	{"events":["lasagna","nachos","brushed teeth","running","weekend"],
-		"squirrel":false},
-	{"events":["potatoes","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["carrot","work"],
-		"squirrel":false},
-	{"events":["pizza","beer","work","dentist"],
-		"squirrel":false},
-	{"events":["lasagna","pudding","cycling","work"],
-		"squirrel":false},
-	{"events":["spaghetti","brushed teeth","reading","work"],
-		"squirrel":false},
-	{"events":["spaghetti","pudding","television","weekend"],
-		"squirrel":false},
-	{"events":["bread","brushed teeth","exercise","weekend"],
-		"squirrel":false},
-	{"events":["lasagna","peanuts","work"],
-		"squirrel":true},
-	{"events":["pizza","work"],
-		"squirrel":false},
-	{"events":["potatoes","exercise","work"],
-		"squirrel":false},
-	{"events":["brushed teeth","exercise","work"],
-		"squirrel":false},
-	{"events":["spaghetti","brushed teeth","television","work"],
-		"squirrel":false},
-	{"events":["pizza","cycling","weekend"],
-		"squirrel":false},
-	{"events":["carrot","brushed teeth","weekend"],
-		"squirrel":false},
-	{"events":["carrot","beer","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["pizza","peanuts","candy","work"],
-		"squirrel":true},
-	{"events":["carrot","peanuts","brushed teeth","reading","work"],
-		"squirrel":false},
-	{"events":["potatoes","peanuts","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["carrot","nachos","brushed teeth","exercise","work"],
-		"squirrel":false},
-	{"events":["pizza","peanuts","brushed teeth","television","weekend"],
-		"squirrel":false},
-	{"events":["lasagna","brushed teeth","cycling","weekend"],
-		"squirrel":false},
-	{"events":["cauliflower","peanuts","brushed teeth","computer","work","touched tree"],
-		"squirrel":false},
-	{"events":["lettuce","brushed teeth","television","work"],
-		"squirrel":false},
-	{"events":["potatoes","brushed teeth","computer","work"],
-		"squirrel":false},
-	{"events":["bread","candy","work"],
-		"squirrel":false},
-	{"events":["potatoes","nachos","work"],
-		"squirrel":false},
-	{"events":["carrot","pudding","brushed teeth","weekend"],
-		"squirrel":false},
-	{"events":["carrot","brushed teeth","exercise","weekend","touched tree"],
-		"squirrel":false},
-	{"events":["brussel sprouts","running","work"],
-		"squirrel":false},
-	{"events":["brushed teeth","work"],
-		"squirrel":false},
-	{"events":["lettuce","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["candy","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["brussel sprouts","brushed teeth","computer","work"],
-		"squirrel":false},
-	{"events":["bread","brushed teeth","weekend"],
-		"squirrel":false},
-	{"events":["cauliflower","brushed teeth","weekend"],
-		"squirrel":false},
-	{"events":["spaghetti","candy","television","work","touched tree"],
-		"squirrel":false},
-	{"events":["carrot","pudding","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["lettuce","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["carrot","ice cream","brushed teeth","cycling","work"],
-		"squirrel":false},
-	{"events":["pizza","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["spaghetti","peanuts","exercise","weekend"],
-		"squirrel":true},
-	{"events":["bread","beer","computer","weekend","touched tree"],
-		"squirrel":false},
-	{"events":["brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["lettuce","peanuts","brushed teeth","work","touched tree"],
-		"squirrel":false},
-	{"events":["lasagna","brushed teeth","television","work"],
-		"squirrel":false},
-	{"events":["cauliflower","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["carrot","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["carrot","reading","weekend"],
-		"squirrel":false},
-	{"events":["carrot","peanuts","reading","weekend"],
-		"squirrel":true},
-	{"events":["potatoes","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["lasagna","ice cream","work","touched tree"],
-		"squirrel":false},
-	{"events":["cauliflower","peanuts","brushed teeth","cycling","work"],
-		"squirrel":false},
-	{"events":["pizza","brushed teeth","running","work"],
-		"squirrel":false},
-	{"events":["lettuce","brushed teeth","work"],
-		"squirrel":false},
-	{"events":["bread","brushed teeth","television","weekend"],
-		"squirrel":false},
-	{"events":["cauliflower","peanuts","brushed teeth","weekend"],
-		"squirrel":false}
-];
-
-function hasEvent(event, entry) {
-
-	return entry.events.indexOf(event) !== -1;
-
-}
-
-function tableFor(event, journal) {
-
-	let table;
-	table = [0, 0, 0, 0];
-
-	for (let i = 0; i < journal.length; i++) {
-
-		let entry = journal[i], index = 0;
-
-		if (hasEvent(event, entry)) index +=1;
-		if (entry.squirrel) index += 2;
-
-		table[index] += 1;
-
-	}
-
-	return table;
-
-}
-console.log(tableFor('pizza', journal));
-
-let map = {};
-function storePhi(event, phi) {
-	map[event] = phi;
-}
-
-storePhi('pizza', 0.069);
-storePhi('touched a tree', -0.081);
-console.log('pizza' in map);
-console.log(map['touched a tree']);
-
-for (let event in map)
-console.log("Correlation for '" + event + "' = " + map[event]);
-
-function gatherCorrelations(journal) {
-	let phis = {};
-	for (let entry = 0; entry < journal.length; entry++) {
-		let events = journal[entry].events;
-		for (let i = 0; i < events.length; i++) {
-			let event = events[i];
-			if (!(event in phis))
-				phis[event] = phi(tableFor(event, journal));
-		}
-	}
-	return phis;
-}
-
-let correlations = gatherCorrelations(journal);
-console.log(correlations.pizza);
+console.log(whatsThis());
