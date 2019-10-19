8d7ac4fc41071d6765acb0557464f92598e213fd
dmytroony <dm.onysko@gmail.com>
2019-10-19 21:51:06 +0300

    General project files Update.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-console.log(this);
-
-function abc() {
-	console.log(this);
-	console.log('Hello world!');
-	this.style.background = 'red';
-}
-
-// document.querySelector('.lorem').onclick = abc;
-
-let p = document.querySelectorAll('p');
-
-// p.forEach(function (element) {
-// 	element.onclick = abc;
-// });
 // console.log(this);
+//
+// function abc() {
+// 	console.log(this);
+// 	console.log('Hello world!');
+// 	this.style.background = 'red';
+// }
+//
+// // document.querySelector('.lorem').onclick = abc;
+//
+// let p = document.querySelectorAll('p');
+//
+// // p.forEach(function (element) {
+// // 	element.onclick = abc;
+// // });
+// // console.log(this);
+//
+// // document.querySelector('.lorem')
+// // 	.addEventListener('click', abc);
+//
+// p.forEach(function (element) {
+// 	element.addEventListener('click', abc);
+// });
 
-// document.querySelector('.lorem')
-// 	.addEventListener('click', abc);
-
-p.forEach(function (element) {
-	element.addEventListener('click', abc);
-});
