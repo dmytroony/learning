35232281f43c6cc9b2c849a46b65c4bc0de7db36
dmytroony <dm.onysko@gmail.com>
2020-02-14 09:55:37 +0200

    was learned today

--- ecmascript/js/script.js
+++ ecmascript/js/script.js
@@ -19,16 +19,63 @@
 // setTimeout(() => console.log('After 10 second'), 10000);
 
 // Context
-function log() {
-    console.log(this);
-}
-const arrowLog = () => console.log(this);
+// function log() {
+//     console.log(this);
+// }
+// const arrowLog = () => console.log(this);
+//
+// const person = {
+//     name: 'Elena',
+//     age: 20,
+//     log: log,
+//     arrowLog: arrowLog,
+// };
+//
+// console.log(person.name);
 
-const person = {
-    name: 'Elena',
-    age: 20,
-    log: log,
-    arrowLog: arrowLog,
-};
+// getHiddenCard
+// need to receive 16digits card's number, and to hide the first 12 digits and add the (*)x4 before the new number
+// const getHiddenCard = (cardNumber, symbolsAmount = 4) => {
+//
+// 	const hiddenCardNumber = cardNumber.toString().slice(-4);
+// 	return hiddenCardNumber.padStart(symbolsAmount + hiddenCardNumber.length, '*');
+//
+// };
+//
+// console.log(getHiddenCard(1234569999999999));
 
-console.log(person.name);
+// to do a first letter toUpperCase
+// ф-я: 1) приймає слово; 2) першу букву слова робить великою; 3) повертає нове слово;
+// const capitalize = name => {
+//
+// 	const firstLetter = name.slice(0, 1).toUpperCase();
+// 	return name.slice(1).padStart(name.length, firstLetter);
+//
+// };
+//
+// EQUAL
+// const capitalize2 = text => `${text[0].toUpperCase()}${text.slice(1)}`;
+// console.log(capitalize2('arya'));
+
+// const isCastle = type => type === 'Castle';
+// console.log(isCastle('Sea'));
+
+// const hasTargaryenReference = (secondName) => {
+//     return secondName.substring(0, secondName.length) === 'Targaryen';
+// };
+// console.log(hasTargaryenReference('Targaryen'));
+
+// isCorrectPassword
+// const isCorrectPassword = (password) => {
+//     const length = password.length;
+//     return length >= 8 && length <= 16 && password[0] === password[0].toUpperCase();
+// };
+// console.log(isCorrectPassword('qwerty'));
+
+
+
+// PLAN
+// 0) code.basics
+// 1) Events
+// 2) Objects
+// 3) DOM
--- js/script.js
+++ js/script.js
@@ -22,38 +22,41 @@
 // 	element.addEventListener('click', abc);
 // });
 
-let classes = {
-	classA:["Tom","Alice"],
-	classB:["Sam","Bob"],
-	classC:["Ron","John"]
-};
+// let classes = {
+// 	classA:["Tom","Alice"],
+// 	classB:["Sam","Bob"],
+// 	classC:["Ron","John"]
+// };
+//
+// for(let key in classes){
+//
+// 	console.log(key+":");
+//
+// 	for(let val of classes[key]){
+// 		console.log(val);
+// 	}
+//
+// }
 
-for(let key in classes){
+// SORTING
+// function BubbleSort(A) {// A - массив, который нужно отсортировать по возрастанию.
+//
+// 	let n = A.length;
+// 	for (let i = 0; i < n-1; i++) {
+//
+// 		for (let j = 0; j < n-1-i; j++)	{
+//
+// 			if (A[j+1] < A[j]) {
+// 				let t = A[j+1]; A[j+1] = A[j]; A[j] = t;
+//
+// 			}
+//
+// 		}
+//
+// 	}
+// 	return A;    // На выходе сортированный по возрастанию массив A.
+// }
+//
+// console.log(BubbleSort([1, 18, 3, 15, 2, 4, 9, 7]));
 
-	console.log(key+":");
 
-	for(let val of classes[key]){
-		console.log(val);
-	}
-
-}
-
-function BubbleSort(A) {// A - массив, который нужно отсортировать по возрастанию.
-
-	let n = A.length;
-	for (let i = 0; i < n-1; i++) {
-
-		for (let j = 0; j < n-1-i; j++)	{
-
-			if (A[j+1] < A[j]) {
-				let t = A[j+1]; A[j+1] = A[j]; A[j] = t;
-
-			}
-
-		}
-
-	}
-	return A;    // На выходе сортированный по возрастанию массив A.
-}
-
-console.log(BubbleSort([1, 18, 3, 15, 2, 4, 9, 7]));
