2dfeed60e2c7f93e4f045a6df63d6126197d3d6a
dmytroony <dm.onysko@gmail.com>
2020-07-22 22:03:56 +0300

    lesson 5 in process, switch

--- csharp/ConsoleAppFirstApp/ConsoleAppFirstApp/Program.cs
+++ csharp/ConsoleAppFirstApp/ConsoleAppFirstApp/Program.cs
@@ -10,12 +10,26 @@ namespace ConsoleAppFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a number:");
+            Console.WriteLine("enter a number 1-3");
             string text = Console.ReadLine();
             int x = Convert.ToInt32(text);
 
-            string result = x > 100 ? "more than 100" : "less then 100";
-            Console.WriteLine("Result: " + result);
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Number 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Number 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Number 3");
+                    break;
+                default:
+                    Console.WriteLine("wtf number");
+                    break;
+            }
+
             Console.ReadKey();
         }
     }
