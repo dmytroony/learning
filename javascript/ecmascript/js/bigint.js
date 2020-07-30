e642fef59ea87e7caef00fced84b10d6c2825cda
dmytroony <dm.onysko@gmail.com>
2020-07-30 18:37:14 +0300

    bigint done

---
+++ javascript/ecmascript/js/bigint.js
@@ -0,0 +1,46 @@
+// console.log('MAX_SAFE_INTEGER: ' + Number.MAX_SAFE_INTEGER);
+// // equal
+// console.log('Math.pow(2, 53) - 1: ' + (Math.pow(2, 53) - 1));
+//
+// console.log('MIN_SAFE_INTEGER: ' + Number.MIN_SAFE_INTEGER);
+// console.log('MAX_VALUE: ' + Number.MAX_VALUE);
+// console.log('MIN_VALUE: ' + Number.MIN_VALUE);
+// console.log('POSITIVE_INFINITY: ' + Number.POSITIVE_INFINITY);
+// console.log('NEGATIVWE_INFINITY: ' + Number.NEGATIVE_INFINITY);
+// console.log('Something / 0: ' + (1 / 0));
+// console.log('typeof NaN(Not a Number): ' + typeof NaN);
+// console.log('Number.NaN: ' + Number.NaN);
+//
+// const weird = 2 / undefined;
+// console.log(Number.isNaN(weird));
+//
+// console.log(Number.isNaN(42));
+// console.log(Number.isFinite(Infinity));
+// console.log(Number.isFinite(42));
+
+// const stringInt = '42';
+// const stringFloat = '42.42';
+// console.log(stringInt + 2);
+// console.log(Number.parseInt(stringInt) + 2);
+// console.log(+stringInt + 2);
+// console.log(stringInt + stringFloat);
+// console.log(parseFloat(stringFloat) + 0.21);
+
+// console.log(.4 + .2);
+// console.log((.4 + .2).toFixed(1));
+
+//
+
+// console.log(9323923902839012838129381023819023819023n);
+// console.log(typeof 9323923902839012838129381023819023819023n);
+
+// console.log(10n - 4n);
+// console.log(10n - BigInt(4));
+
+// console.log(Math.random().toFixed(1));
+
+// function getRandomBetween (min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+//
+// console.log(getRandomBetween(1, 5).toFixed());
