34b688fee6da14db2736f8c05d449794da712acd
dmytroony <dm.onysko@gmail.com>
2020-08-03 11:59:53 +0300

    objects refreshing

--- javascript/ecmascript/js/objects.js
+++ javascript/ecmascript/js/objects.js
@@ -108,7 +108,7 @@
 //     },
 // };
 
-// clone with ... (spead operator)
+// clone with ... (spread operator)
 // let clonedUser = { ...user };
 // clonedUser.sizes.height = 192;
 
@@ -191,7 +191,7 @@
 // let user = {
 //     name: 'Dmytro',
 // };
-// let id = Symbol('identificator');
+// let id = Symbol('identifier');
 // let idAgain = Symbol.for('id');
 // user[id] = 1;
 
@@ -525,3 +525,25 @@
 // };
 // obj.display();
 // obj.print();
+
+const person = {
+    name: 'Dmytro',
+    age: 30,
+    isProgrammer: true,
+    languages: ['ua', 'ru', 'en'],
+    'complex key': 'Complex value',
+    ['key_' + (1 + 3)]: 'Computed key',
+    greet() {
+        console.log('greet from person');
+    },
+};
+
+console.log(person.name);
+const ageKey = 'age';
+console.log(person[ageKey]);
+console.log(person['complex key']);
+console.log(person);
+person.greet();
+
+delete person['key_4'];
+console.log(person);
