12bfd6a3d52e0dae9e6434b48f9c83e40bdb38c7
dmytroony <dm.onysko@gmail.com>
2020-09-01 22:39:35 +0300

    4project in process. Created Navbar, added Routing, created About component

---
+++ javascript/frameworks/react/todo4app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React, {} from "react";
+
+export const About = () => (
+	<div className="jumbotron">
+		<div className="container">
+			<h1 className="display-4">The best React App</h1>
+			<p className="lead">Latest Version <strong>2.3.4.45</strong></p>
+			<p className="lead">LTS Version <strong>2.3.3.2</strong></p>
+		</div>
+	</div>
+)
+
