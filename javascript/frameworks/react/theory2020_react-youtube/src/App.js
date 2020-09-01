3db1b1b83cdb348c5fa6bcd37b35f9f9e7399774
dmytroony <dm.onysko@gmail.com>
2020-09-01 17:13:06 +0300

    lesson in process. Created a custom hook, useEffect, work with the Server(jsonplaceholder.typicode.com)

--- javascript/frameworks/react/theory2020_react-youtube/src/App.js
+++ javascript/frameworks/react/theory2020_react-youtube/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList';
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
 
 function App() {
-	const [todos, setTodos] = React.useState([
-		{id: 1, completed: false, title: 'Buy bread'},
-		{id: 2, completed: false, title: 'Buy oil'},
-		{id: 3, completed: false, title: 'Buy milk'},
-	]);
+	const [todos, setTodos] = React.useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+			.then(response => response.json())
+			.then(todos => {
+				setTimeout(() => {
+					setTodos(todos);
+				}, 2000);
+			})
+	}, [])
 
 	function toggleTodo(id) {
 		setTodos(todos.map(todo => {
