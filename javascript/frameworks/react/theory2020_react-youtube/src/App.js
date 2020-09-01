4fb715e17fcc8332664746350e3501b4be6be45e
dmytroony <dm.onysko@gmail.com>
2020-09-01 15:38:27 +0300

    lesson in process. Components, Styles, Cycles, PropTypes, JSX

--- javascript/frameworks/react/theory2020_react-youtube/src/App.js
+++ javascript/frameworks/react/theory2020_react-youtube/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import TodoList from './Todo/TodoList';
 
 function App() {
+	const todos = [
+		{id: 1, completed: false, title: 'Buy bread'},
+		{id: 2, completed: false, title: 'Buy oil'},
+		{id: 3, completed: false, title: 'Buy milk'},
+	];
+
 	return (
 		<div className='wrapper'>
 			<h1>React tutorial</h1>
-			<TodoList />
+			<TodoList todos={todos} />
 		</div>
 	);
 }
