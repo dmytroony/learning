e7efee4e5bdc6960af3dbfe962d2330f1a113d1c
dmytroony <dm.onysko@gmail.com>
2020-09-15 17:56:59 +0300

    the react todo4app in process. created all components, connected to firebase.google.com an created a cloud DB, added styles, added visual indicator Spinner, created alerts, other. Added a new package for animation - react-transition-group

--- javascript/frameworks/react/todo4app/src/pages/Home.js
+++ javascript/frameworks/react/todo4app/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useContext, useEffect} from "react";
+import { Form } from "../components/Form";
+import { Notes } from "../components/Notes";
+import {FirebaseContext} from "../context/firebase/firebaseContext";
+import {Loader} from "../components/Loader";
 
 export const Home = () => {
+	const {loading, notes, fetchNotes, removeNote} = useContext(FirebaseContext);
+
+	useEffect(() => {
+		fetchNotes()
+		// eslint-disable-next-line
+	}, [])
+
 	return (
 		<Fragment>
-			<h1>Home Page</h1>
+			<Form />
+			<hr />
+
+			{loading
+				? <Loader /> : <Notes notes={notes} onRemove={removeNote} />
+			}
+
 		</Fragment>
 	)
 }
