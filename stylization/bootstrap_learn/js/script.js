0c796cbb0d400f849fb4c13e6895790867b67432
dmytroony <dm.onysko@gmail.com>
2020-09-18 15:53:02 +0300

    Bootstrap4. Lesson5 done. Modal window added

--- stylization/bootstrap_learn/js/script.js
+++ stylization/bootstrap_learn/js/script.js
@@ -1,5 +1,10 @@
 $('.carousel').carousel({
 	interval: 3000,
-	// keyboard: false,
+	// keyboard: false, // use left - right
 	// wrap: true, // stop slider when the last img
 });
+
+$('#exampleModal').modal({
+	keyboard: false, // Esc, etc.
+	backdrop: "static", // close modal to press on behind of modal
+})
