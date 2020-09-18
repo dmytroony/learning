c796b2a20b7051190f906367cfb1f07176119938
dmytroony <dm.onysko@gmail.com>
2020-09-18 17:00:41 +0300

    Bootstrap4. Lesson6 done. Added the Login form

--- stylization/bootstrap_learn/js/script.js
+++ stylization/bootstrap_learn/js/script.js
@@ -4,7 +4,7 @@ $('.carousel').carousel({
 	// wrap: true, // stop slider when the last img
 });
 
-$('#exampleModal').modal({
-	keyboard: false, // Esc, etc.
-	backdrop: "static", // close modal to press on behind of modal
-})
+// $('#exampleModal').modal({
+// 	keyboard: false, // Esc, etc.
+// 	backdrop: "static", // close modal to press on behind of modal
+// });
