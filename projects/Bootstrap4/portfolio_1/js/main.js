7fc0e2744d2c15e2ad300df55fc91619330d59a2
dmytroony <dm.onysko@gmail.com>
2020-09-22 20:18:54 +0300

    B4_P_P1 in process. Creates StickyNav function for the NavBar top menu

--- projects/Bootstrap4/portfolio_1/js/main.js
+++ projects/Bootstrap4/portfolio_1/js/main.js
@@ -40,4 +40,23 @@ $(document).ready(function () {
         }
     });
 
+
+    // Sticky-NavMenu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
 });
