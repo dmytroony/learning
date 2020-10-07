85a0d003523d78f0ef2282485c8ff29566da14c5
dmytroony <dm.onysko@gmail.com>
2020-10-07 11:23:55 +0300

    bug-tracker lesson1 done

---
+++ projects/React/bug-tracker/src/Controllers/Redux/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "user",
+  initialState: [{}],
+  reducers: {
+    getUser: state => {
+      state.push({ name: "Dmytro Onysko" });
+      state.push({ name: "John Doe" });
+    }
+  }
+});
+
+export default slice.reducer;
+
+export const { getUser } = slice.actions;
