8e3e61a8962c9bdd000a97d0fc9e20c223cb4e1d
dmytroony <dm.onysko@gmail.com>
2020-10-14 15:51:40 +0300

    VUE crash-course in process

--- javascript/frameworks/vue/crash-course/src/App.vue
+++ javascript/frameworks/vue/crash-course/src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Todo application</h1>
+    <AddTodo @add-todo="addTodo" />
+    <hr />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
 
 export default {
-  name: "App",
+  name: "app",
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Buy a bread", completed: false },
+        { id: 2, title: "Sell a car", completed: false },
+        { id: 3, title: "Make the Todo app", completed: false }
+      ]
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json;
+      });
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    }
+  },
   components: {
-    HelloWorld
+    TodoList,
+    AddTodo
   }
 };
 </script>
