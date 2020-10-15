3340c352d69766c498ecad2122a5db067b36acdf
dmytroony <dm.onysko@gmail.com>
2020-10-15 16:47:30 +0300

    fullstack rest-api app in process: set up the server

--- projects/fullstack/vue-node/contacts-app/app.js
+++ projects/fullstack/vue-node/contacts-app/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.listen();
+// Server side
+app.use(express.static(path.resolve(__dirname, "client"))); // making the client folder as a static
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
+});
+
+app.listen(3000, () => console.log("Server has been started on port 3000..."));
