70838dfeac022bab5638e70803aff31459f6816e
dmytroony <dm.onysko@gmail.com>
2020-10-15 23:31:13 +0300

    nodejs crash-course in process

--- javascript/frameworks/nodejs/lesson-by-vm/index.js
+++ javascript/frameworks/nodejs/lesson-by-vm/index.js
@@ -1,1 +1,14 @@
-console.log("Hello NodeJS");
+// (function (exports, require, module, __dirname, __filename) {
+//   const chalk = require('chalk')
+//   const text = require('./data')
+//
+//   console.log(chalk.blue(text))
+//   console.log(__dirname)
+// })
+
+const chalk = require("chalk");
+const text = require("./data");
+
+console.log(chalk.blue(text));
+console.log(__dirname);
+console.log(__filename);
