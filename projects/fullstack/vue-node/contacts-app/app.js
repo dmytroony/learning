e1f30de24fd9b6fc2edd31eb8bf69312484cac7f
dmytroony <dm.onysko@gmail.com>
2020-10-15 18:30:59 +0300

    fullstack rest-api app in process: realized get requests, added loader, installed the uuid npm package

--- projects/fullstack/vue-node/contacts-app/app.js
+++ projects/fullstack/vue-node/contacts-app/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const path = require("path");
+const { v4 } = require("uuid"); // auto-generating id
 const app = express();
 
-// Server side
+const CONTACTS = [
+  { id: v4(), name: "Dmytro", value: "+380958052625", marked: false }
+];
+
+// GET
+app.get("/api/contacts", (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS);
+  }, 1000);
+});
+
+// POST
+app.post("/api/contacts", (req, res) => {});
+
+// Server side settings
 app.use(express.static(path.resolve(__dirname, "client"))); // making the client folder as a static
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
+// Listen the 3000 port
 app.listen(3000, () => {
   console.log("Server has been started on port 3000...");
 });
--- projects/fullstack/vue-node/contacts-app/client/frontend.js
+++ projects/fullstack/vue-node/contacts-app/client/frontend.js
@@ -1,16 +1,23 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
 
+Vue.component("loader", {
+  template: `<div style="display: flex; justify-content: center; align-items: center;">
+        <div class="spinner-border text-info" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>`
+});
+
 new Vue({
   el: "#app",
   data() {
     return {
+      loading: false,
       form: {
         name: "",
         value: ""
       },
-      contacts: [
-        { id: 1, name: "Dmytro", value: "+380958052625", marked: false }
-      ]
+      contacts: []
     };
   },
   computed: {
@@ -19,7 +26,6 @@ new Vue({
     }
   },
   methods: {
-    contacts: undefined,
     createContact() {
       const { ...contact } = this.form;
 
@@ -34,5 +40,32 @@ new Vue({
     removeContact(id) {
       this.contacts = this.contacts.filter(c => c.id !== id);
     }
+  },
+  async mounted() {
+    this.loading = true;
+    this.contacts = await request("/api/contacts");
+    this.loading = false;
   }
 });
+
+async function request(url, method = "GET", data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    // has data?
+    if (data) {
+      headers["Content-Type"] = "application/json";
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    });
+    return await response.json();
+  } catch (e) {
+    console.warn("Error", e.message);
+  }
+}
