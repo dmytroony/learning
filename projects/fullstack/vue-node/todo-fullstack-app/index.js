37e0a886b39e639ccf3a2235cfb96a0d48b70ea9
dmytroony <dm.onysko@gmail.com>
2020-10-17 12:12:39 +0300

    fullstack todo app in process. divided app on parts with handlebars, added routes for pages

---
+++ projects/fullstack/vue-node/todo-fullstack-app/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const todoRoutes = require("./routes/todos");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs"
+});
+
+// Handlebars engine
+app.engine("hbs", hbs.engine); // Register the engine with the hbs key
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(todoRoutes);
+
+async function start() {
+  try {
+    // Connect DB
+    await mongoose.connect(
+      "mongodb+srv://dmytroony:2XMmNQpF975s3n5@cluster0.ot80v.mongodb.net/todos",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      }
+    );
+    app.listen(PORT, () => {
+      // Connect server
+      console.log(`Server has been started on Port:${PORT}`);
+    });
+  } catch (e) {
+    console.log(e); // error
+  }
+}
+
+start();
