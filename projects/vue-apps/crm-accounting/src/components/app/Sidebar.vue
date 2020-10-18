bfa80ea403e003c1a6d3d225977a7db342914604
dmytroony <dm.onysko@gmail.com>
2020-10-18 15:33:54 +0300

    vue crm-acc app in process. divided main layout. created and fixed the sidebar component. added routing to him

---
+++ projects/vue-apps/crm-accounting/src/components/app/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="/" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { title: "Account", url: "/", exact: true },
+      { title: "History", url: "/history" },
+      { title: "Planning", url: "/planning" },
+      { title: "New entry", url: "/record" },
+      { title: "Categories", url: "/categories" }
+    ]
+  })
+};
+</script>
--- projects/vue-apps/crm-accounting/src/layouts/MainLayout.vue
+++ projects/vue-apps/crm-accounting/src/layouts/MainLayout.vue
@@ -1,69 +1,9 @@
 <template>
   <div>
     <div class="app-main-layout">
-      <nav class="navbar orange lighten-1">
-        <div class="nav-wrapper">
-          <div class="navbar-left">
-            <a href="#">
-              <i class="material-icons black-text">dehaze</i>
-            </a>
-            <span class="black-text">12.12.12</span>
-          </div>
+      <Navbar />
 
-          <ul class="right hide-on-small-and-down">
-            <li>
-              <a
-                class="dropdown-trigger black-text"
-                href="#"
-                data-target="dropdown"
-              >
-                USER NAME
-                <i class="material-icons right">arrow_drop_down</i>
-              </a>
-
-              <ul id="dropdown" class="dropdown-content">
-                <li>
-                  <a href="#" class="black-text">
-                    <i class="material-icons">account_circle</i>Profile
-                  </a>
-                </li>
-                <li class="divider" tabindex="-1"></li>
-                <li>
-                  <a href="#" class="black-text">
-                    <i class="material-icons">assignment_return</i>Log out
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <ul class="sidenav app-sidenav open">
-        <li>
-          <a href="/" class="waves-effect waves-orange pointer">Account</a>
-        </li>
-        <li>
-          <a href="/history" class="waves-effect waves-orange pointer"
-            >History</a
-          >
-        </li>
-        <li>
-          <a href="/planning" class="waves-effect waves-orange pointer"
-            >Planning</a
-          >
-        </li>
-        <li>
-          <a href="/record" class="waves-effect waves-orange pointer"
-            >New entry</a
-          >
-        </li>
-        <li>
-          <a href="/categories" class="waves-effect waves-orange pointer"
-            >Categories</a
-          >
-        </li>
-      </ul>
+      <Sidebar />
 
       <main class="app-content">
         <div class="app-page">
@@ -79,3 +19,16 @@
     </div>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
+
+export default {
+  name: "main-layout",
+  components: {
+    Navbar,
+    Sidebar
+  }
+};
+</script>
