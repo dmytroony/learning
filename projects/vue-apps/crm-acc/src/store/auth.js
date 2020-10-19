409afb1e392a4fea54c91ab79c7db4f8bf9e5736
dmytroony <dm.onysko@gmail.com>
2020-10-19 19:01:02 +0300

    added user registration and adding his to the firebaseDB with start fields bill and name. also added users check in system

--- projects/vue-apps/crm-acc/src/store/auth.js
+++ projects/vue-apps/crm-acc/src/store/auth.js
@@ -9,6 +9,25 @@ export default {
         throw err;
       }
     },
+    async register({ dispatch }, { email, password, name }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+        const uid = await dispatch("getUserId");
+        await firebase
+          .database()
+          .ref(`/users/${uid}/info`)
+          .set({
+            bill: 100,
+            name
+          });
+      } catch (err) {
+        throw err;
+      }
+    },
+    getUserId() {
+      const user = firebase.auth().currentUser;
+      return user ? user.uid : null;
+    },
     async logout() {
       await firebase.auth().signOut();
     }
--- projects/vue-apps/crm-acc/src/views/Register.vue
+++ projects/vue-apps/crm-acc/src/views/Register.vue
@@ -118,7 +118,7 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -128,8 +128,11 @@ export default {
         password: this.password,
         name: this.name
       };
-      console.log(formData);
-      this.$router.push("/");
+
+      try {
+        await this.$store.dispatch("register", formData);
+        this.$router.push("/");
+      } catch (err) {}
     }
   }
 };
