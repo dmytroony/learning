570f9d3f29c7ab9a2a0cea72cb5d43ee8fed0b1e
dmytroony <dm.onysko@gmail.com>
2020-10-19 17:53:25 +0300

    prepared for firebase

--- projects/vue-apps/crm-acc/src/main.js
+++ projects/vue-apps/crm-acc/src/main.js
@@ -8,13 +8,36 @@ import messagePlugin from "@/utils/message.plugin";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false;
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+firebase.initializeApp({
+  apiKey: "AIzaSyA5PQYOzD7NfgZmi7lov7olQFR6lsvS8jg",
+  authDomain: "vue-crm-acc.firebaseapp.com",
+  databaseURL: "https://vue-crm-acc.firebaseio.com",
+  projectId: "vue-crm-acc",
+  storageBucket: "vue-crm-acc.appspot.com",
+  messagingSenderId: "147713150742",
+  appId: "1:147713150742:web:5234bc1411a73574298a37",
+  measurementId: "G-VQSY23KP4X"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
+
+// firebase.analytics();
