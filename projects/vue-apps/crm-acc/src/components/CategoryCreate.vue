775f5aee0874ccad6b02a56ff4b0d0c563899ce1
dmytroony <dm.onysko@gmail.com>
2021-02-14 21:50:53 +0200

    some changes

---
+++ projects/vue-apps/crm-acc/src/components/CategoryCreate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Create</h4>
+      </div>
+
+      <form>
+        <div class="input-field">
+          <input id="name" type="text" />
+          <label for="name">Name</label>
+          <span class="helper-text invalid">Enter the name</span>
+        </div>
+
+        <div class="input-field">
+          <input id="limit" type="number" />
+          <label for="limit">Limit</label>
+          <span class="helper-text invalid">Minimum value</span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Create
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
