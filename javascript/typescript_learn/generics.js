c464c42a02d80107d5a612831c2cf4e4fcddbf89
dmytroony <dm.onysko@gmail.com>
2021-03-02 00:08:34 +0200

    update. typescript crachcourse done

---
+++ javascript/typescript_learn/generics.js
@@ -0,0 +1,8 @@
+var arrayOfNumbers = [1, 1, 2, 3, 5];
+var arrayOfStrings = ["Hello", "Dmytro"];
+function reverse(array) {
+  return array.reverse();
+}
+console.log(reverse(arrayOfNumbers));
+console.log(reverse(arrayOfStrings));
+//# sourceMappingURL=generics.js.map
