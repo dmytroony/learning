688ce7646bba4a0b6018c46500f4fd1f6eb989a3
Dmytro Onysko <dmytroony@gmail.com>
2021-03-29 17:17:10 +0300

    modal window added

---
+++ projects/materialize-css-v1/learncodeonline-crashcourse/js/loaders.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.parallax').parallax();
+});
+
+function toggleModal() {
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open();
+}
+
