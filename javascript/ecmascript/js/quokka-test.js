aea286bc8b5be8356eaa8f7f2f756e60cc0104ba
Dmytro Onysko <dmytroony@gmail.com>
2021-04-06 20:33:13 +0300

    quokka review

---
+++ javascript/ecmascript/js/quokka-test.js
@@ -0,0 +1,71 @@
+// const axios = require('axios');
+
+// async function getData() {
+//     const {data} = await axios.get('https://reqres.in/api/users?page=2');
+//     data
+// }
+
+// getData();
+
+// const a = 23;
+
+// let result = a ** 2;
+
+// const randomNum = Math.random();
+
+// console.log(randomNum);
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3,
+// };
+
+// let oP = Object.prototype;
+// let newO = new Object;
+
+// console.log(oP);
+// console.log(newO);
+
+// console.log(document);
+// console.log(Document);
+
+
+// console.log(Math.random());
+// console.log(Math.abs(-122));
+
+// let newDate = new Date().toDateString();
+// console.log(newDate);
+
+// const locale = 'uk-UA';
+
+// const options = {
+//     day: 'numeric',
+//     year: 'numeric',
+//     month: 'short', // long
+// };
+
+// const date = new Intl.DateTimeFormat(locale, options).format();
+
+// console.log(date);
+// console.log(options);
+
+// const fib = [1, 1, 2, 3, 5, 8, 13, 21, 34,];
+
+// const nextFib = fib.concat([55]);
+
+// const next = [...nextFib, 89, 144];
+
+// console.log(next);
+
+// console.log(next.every(n => typeof n === 'number'));
+
+// console.log(next.findIndex(n => n === 5));
+
+// console.log(
+//     next
+//     .map(num => num ** 2)
+//     .filter(num => num % 2 === 0)
+//     .reduce((acc, n) => acc + n, 0)
+// );
+
