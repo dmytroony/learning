236523a6fdff3e9d60667e12517df7b45b75eabb
Dmytro Onysko <dmytroony@gmail.com>
2021-05-30 20:41:16 +0300

    python learning update

---
+++ python/pythonworld/start.py
@@ -0,0 +1,59 @@
+# print("Hello" + " " + "World")
+# print(2 + (3 * 5))
+# print(2 + 2)
+# name = input("What is your name?")
+# print("Hello,", name)
+
+# a = 1
+# b = 2
+# c = 3
+# d = 4
+#
+# if (a == 1 and b == 2 and
+#         c == 3 and d == 4):
+#     print(a, b, c, d)
+#     print('spam' * 3)
+
+# x = 2
+# y = 3
+#
+# # if x < y: print(x)
+#
+# print(x and y) if x else print(x or y)
+
+# if x:
+#     print(x and y)
+# else:
+#     print(x or y)
+# print(not x)
+
+# i = 5
+# while i <= 15:
+#     print(i)
+#     i = i + 2
+# print(i)
+
+
+# STR = 'hello world'
+
+# for i in STR:
+#     print(i * 2, end="")
+#
+# print('\n')
+# print(STR)
+
+# for i in STR:
+#     if i == 'o':
+#         continue
+#     print(i * 2, end="")
+
+# for i in STR:
+#     if i == 'o':
+#         break
+#     print(i * 2, end="")
+
+# for i in STR:
+#     if i == "a":
+#         break
+# else:
+#     print('here is no "a" letter')
