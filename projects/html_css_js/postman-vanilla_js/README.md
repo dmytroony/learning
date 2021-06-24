2b83dd7b6943760ba5a083b1ce68bac82adb7a46
Dmytro Onysko <dmytroony@gmail.com>
2021-06-24 16:30:03 +0300

    added few features

--- projects/html_css_js/postman-vanilla_js/README.md
+++ projects/html_css_js/postman-vanilla_js/README.md
@@ -1,24 +1,29 @@
 # Writing a Postman replica
 
 ###### "[Postman is](https://bit.ly/2Um0Mxg) an API client that makes it easy for developers to create, share, test and document APIs."
+<br>
 
-#### Techstack:
+>#### Techstack:
 * HTML;
 * CSS;
 * JS (plain);
 * NPM
-    * axios
-    * bootstrap
+  * [axios](https://www.npmjs.com/package/axios)
+  * [bootstrap](https://www.npmjs.com/package/bootstrap)
+  * [snowpack](https://www.npmjs.com/package/snowpack)
 
-#### Commands used:
-> `npm init`
->
-> `npm i axios`
->
-> `npm i --save-dev snowpack`
->
-> `npm i bootstrap`
->
-> `npm i @popperjs/core`
-> 
-> `npm start`
+<br>
+
+>#### Commands used:
+
+`npm init`
+
+`npm i axios`
+
+`npm i --save-dev snowpack`
+
+`npm i bootstrap`
+
+`npm i @popperjs/core`
+
+`npm start`
--- projects/html_css_js/postman-vanilla_js/js/script.js
+++ projects/html_css_js/postman-vanilla_js/js/script.js
@@ -1,3 +1,44 @@
 // imports
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// getting the tabs by
+const queryParamsContainer = document
+    .querySelector('[data-query-params]');
+const requestHeadersContainer = document
+    .querySelector('[data-request-headers]');
+const keyValueTemplate = document
+    .querySelector('[data-key-value-template]');
+
+// adding listeners to the tabs  on click [Add] buttons
+document
+    .querySelector('[data-add-query-param-btn]')
+    .addEventListener('click', () => {
+        queryParamsContainer.append(createDeleteKeyValuePair());
+    });
+document
+    .querySelector('[data-add-request-header-btn]')
+    .addEventListener('click', () => {
+        requestHeadersContainer.append(createDeleteKeyValuePair());
+    });
+
+// creating the new [key-value-pairs] elements
+// in chosen tab on the html page
+queryParamsContainer.append(createDeleteKeyValuePair());
+requestHeadersContainer.append(createDeleteKeyValuePair());
+
+// create or delete a key-value pair
+function createDeleteKeyValuePair() {
+    // creating by click on [Add]
+    const element = keyValueTemplate.content.cloneNode(true);
+    // deleting by click on [Remove]
+    element
+        .querySelector('[data-remove-btn]')
+        .addEventListener('click', (e) => {
+            e.target
+                .closest('[data-key-value-pair]').remove();
+        });
+
+    return element;
+}
+
