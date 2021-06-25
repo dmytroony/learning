ca61d9762340c20a681c08d54da3719bf084871f
Dmytro Onysko <dmytroony@gmail.com>
2021-06-25 21:37:53 +0300

    axios added & configured, worked with form & form methods

--- projects/html_css_js/postman-vanilla_js/js/script.js
+++ projects/html_css_js/postman-vanilla_js/js/script.js
@@ -1,6 +1,7 @@
 // imports
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 // getting the tabs by
 const queryParamsContainer = document
@@ -10,7 +11,9 @@ const requestHeadersContainer = document
 const keyValueTemplate = document
     .querySelector('[data-key-value-template]');
 
-// adding listeners to the tabs  on click [Add] buttons
+const form = document.querySelector('[data-form]');
+
+// adding listeners to the tabs on click [Add] buttons
 document
     .querySelector('[data-add-query-param-btn]')
     .addEventListener('click', () => {
@@ -27,6 +30,19 @@ document
 queryParamsContainer.append(createDeleteKeyValuePair());
 requestHeadersContainer.append(createDeleteKeyValuePair());
 
+// form listener for the form fields to click on [Send]
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairsToObjects(queryParamsContainer),
+        headers: keyValuePairsToObjects(requestHeadersContainer),
+    })
+        .then(response => console.log(response));
+});
+
 // create or delete a key-value pair
 function createDeleteKeyValuePair() {
     // creating by click on [Add]
@@ -42,3 +58,16 @@ function createDeleteKeyValuePair() {
     return element;
 }
 
+// converting pairs to objects
+function keyValuePairsToObjects(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]');
+
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value;
+        const value = pair.querySelector('[data-value]').value;
+
+        if (key === '') return data;
+
+        return { ...data, [key]: value, };
+    }, {});
+}
