bbbfcddf8ce22644c37a906a89c0b4692cc1e39f
Dmytro Onysko <dmytroony@gmail.com>
2023-09-17 22:02:20 +0300

    pictures script

--- javascript/ecmascript/jsdeepdive/api/js/pics.js
+++ javascript/ecmascript/jsdeepdive/api/js/pics.js
@@ -1,17 +1,25 @@
-exports.handler = function (context, event, callback) {
-  const gallery = [
-    {
-      src: "https://placekitten.com/200/300",
-      description: "Look at this kitteh",
-      alt: "A kitteh",
-      thumbnailWidth: "200px",
-    },
-    {
-      src: "https://placekitten.com/300/300",
-      description: "Another Kitteh",
-      alt: "Cutie",
-      thumbnailWidth: "200px",
-    },
-  ];
+exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient();
+  const gallery = [];
+  //   {
+  //     src: "https://placekitten.com/200/300",
+  //     description: "Look at this kitteh",
+  //     alt: "A kitteh",
+  //     thumbnailWidth: "200px",
+  //   },
+  // ];
+  const messages = await client.messages.list({to: context.TWILIO_NUMBER});
+  for (const message of messages) {
+    const pics = message.media().list();
+    for (const pic of pics) {
+      gallery.push({
+        src: "https://api.twilio.com" +
+          pic.uri.replace(".json", ""),
+        description: message.body,
+        alt: message.body,
+        thumbnailWidth: "200px",
+      });
+    }
+  }
   return callback(null, gallery);
 };
