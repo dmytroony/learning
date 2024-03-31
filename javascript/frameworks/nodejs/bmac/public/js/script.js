6b1fe6185a6216f6d43097d69da6ef310dfa532e
Dmytro Onysko <dmytroony@gmail.com>
2024-03-31 23:38:43 +0300

    counter scripted

---
+++ javascript/frameworks/nodejs/bmac/public/js/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function (){
+  // counter
+  const $decrementBtn = document.querySelector('[data-decrement-btn]');
+  const $incrementBtn = document.querySelector('[data-increment-btn]');
+  const $counterField = document.querySelector('[data-counter-field]');
+  const $total = document.querySelector('[data-total]');
+  const minValue = 1;
+  const maxValue = 99999999;
+
+  // ++
+  $incrementBtn.addEventListener('click', function () {
+    const currentValue = $counterField.value;
+    if (currentValue < maxValue) $counterField.value = Number(currentValue) + 1;
+    updateTotal.call($counterField);
+  });
+
+  // --
+  $decrementBtn.addEventListener('click', function () {
+    const currentValue = $counterField.value;
+    if (currentValue > minValue) $counterField.value = Number(currentValue) - 1;
+    updateTotal.call($counterField);
+  });
+
+  const updateTotal = function () {
+    $total.textContent = this.value;
+  }
+
+  $counterField.addEventListener('input', updateTotal.bind($counterField));
+
+})();
