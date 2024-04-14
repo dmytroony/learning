7d0482c06ec576e90da38f167eb36b6c4a2c5d1d
Dmytro Onysko <dmytroony@gmail.com>
2024-04-14 23:15:49 +0300

    challenge-2 done

--- javascript/typescript/practical-ts/README.md
+++ javascript/typescript/practical-ts/README.md
@@ -7,5 +7,9 @@
 - Create a variable of type string and try to invoke a string method on it.
 - Create a variable of type number and try to perform a mathematical operation on it.
 - Create a variable of type boolean and try to perform a logical operation on it.
-- Try to assign a value of a different type to each of these variables and observe the TypeScriopt compiler's response.
+- Try to assign a value of a different type to each of these variables and observe the TypeScript compiler's response.
 - You can use type annotation or inference.
+
+## Challenge 2
+- Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+- Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
--- javascript/typescript/practical-ts/src/tutorial.ts
+++ javascript/typescript/practical-ts/src/tutorial.ts
@@ -1,11 +1,11 @@
 // ## Challenge 1
-let greeting: string = "Hello, TypeScript!";
+// let greeting: string = "Hello, TypeScript!";
 // console.log(greeting.toUpperCase());
 
-let age: number = 25;
+// let age: number = 25;
 // console.log(age + 5);
 
-let isAdult: boolean =  age >= 18;
+// let isAdult: boolean = age >= 18;
 // console.log(isAdult);
 
 // greeting = 10;
@@ -13,3 +13,51 @@ let isAdult: boolean =  age >= 18;
 // isAdult = 'yes';
 //
 // console.log(greeting, age, isAdult);
+
+
+// UNIONS
+// let tax: number | string = 10;
+// console.log(tax);
+// tax = '$10';
+// console.log(tax);
+
+
+// let requestStatus: "pending" | "success" | "error" = "pending";
+// console.log(requestStatus);
+// requestStatus = "error";
+// console.log(requestStatus);
+
+
+// let notSure: any = 4;
+// console.log(notSure);
+// notSure = "a string";
+// notSure = false;
+// console.log(notSure);
+
+
+// const books: string[] = ['1984', 'Brave New World', 'Fahrenheit 451',];
+// let foundBook: string | undefined;
+//
+// for (let book of books) {
+//   if (book === '1984') {
+//     foundBook = book;
+//     foundBook = foundBook.toUpperCase();
+//     break;
+//   }
+// }
+//
+// console.log(foundBook?.length);
+
+
+// ## Challenge 2
+let discount: number | string = 20;
+console.log(discount);
+discount = '20%';
+console.log(discount);
+// discount = true;
+// console.log(discount);
+
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+console.log(orderStatus);
