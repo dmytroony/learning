030e15de04f622d46c1b6ef306244b2adce9d93d
Dmytro Onysko <dmytroony@gmail.com>
2024-04-23 06:42:49 +0300

    renewing cpp knowledges. lessons by David Frazier

---
+++ cpp/firstprogram.cpp
@@ -0,0 +1,19 @@
+// Hello World program
+
+/*
+ * Commands used:
+ * ```g++ filename``` => a.out
+ * ```g++ -o filename.exe filename.cpp``` => filename.exe
+ * ```more filename.exe```, ```more filename.cpp```
+ * ```./filename[exe, cpp, or without an extension]``` 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
