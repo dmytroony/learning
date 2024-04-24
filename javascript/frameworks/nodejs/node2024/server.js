53c6a3f7a6dcdf3d3263e9e310e03b2c1f1ef953
Dmytro Onysko <dmytroony@gmail.com>
2024-04-24 23:15:45 +0300

    playing with headers on the server

--- javascript/frameworks/nodejs/node2024/server.js
+++ javascript/frameworks/nodejs/node2024/server.js
@@ -2,8 +2,15 @@ import http from 'http';
 const PORT = 5237;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello, NodeJS!");
-  res.end();
+  // res.write("Hello, NodeJS!");
+  // res.end("Hello, NodeJS!");
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 404;
+  // res.end("<h1>Hello, NodeJS!</h1>");
+
+  res.writeHead(500, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify( { message: 'Server Error' }, null, 2));
 });
 
 server.listen(PORT, () => {
