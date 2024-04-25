fc0a79751a5aa44c513adbe65a5f974c1e90b383
Dmytro Onysko <dmytroony@gmail.com>
2024-04-25 21:54:59 +0300

    node simple routing

--- javascript/frameworks/nodejs/node2024/server.js
+++ javascript/frameworks/nodejs/node2024/server.js
@@ -11,12 +11,33 @@ const server = http.createServer((req, res) => {
   // res.end("<h1>Hello, NodeJS!</h1>");
   // res.writeHead(500, { 'Content-Type': 'application/json' });
   // res.end(JSON.stringify( { message: 'Server Error' }, null, 2));
+  // console.log(req.url);
+  // console.log(req.method);
+  
+  // simple routing
+  try {
+    if(req.method === 'GET') {  //check if GET
+      if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homepage</h1>');
+      } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>About Us</h1>');
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404</h1><h2>Not found</h2>');
+      }
+    } else {
+      throw new Error("Method not allowed!");
+    }
+  } catch (error) {
+    console.error(error); // check if another method such as POST etc.
+    res.writeHead(500, { 'Content-Type': 'text/html' });
+    res.end('<h1>500</h1><h2><b>ISE</b>: Internal Server Error</h2>');
+  }
 
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello NodeJS!</h1>');
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.end('<h1>Hello NodeJS!</h1>');
 });
 
 server.listen(PORT, () => {
