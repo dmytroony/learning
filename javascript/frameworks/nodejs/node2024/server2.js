ed294efa8df94c7c367ebdf1287d880a7cfe4ec9
Dmytro Onysko <dmytroony@gmail.com>
2024-05-02 00:33:07 +0300

    nodejs. env port bug fixed

--- javascript/frameworks/nodejs/node2024/server2.js
+++ javascript/frameworks/nodejs/node2024/server2.js
@@ -1,1 +1,22 @@
 import { createServer } from 'http';
+const PORT = process.env.PORT;
+
+console.log(PORT);
+
+const users = [
+  { id: 1, name: "John",  },
+  { id: 3, name: "Jane",  },
+  { id: 3, name: "Bill",  },
+];
+
+const server = createServer((req, res) => {
+  if (req.url === '/api/users' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(users));
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
