9449b26d9c523424dc2306847ecef56043f1c245
Dmytro Onysko <dmytroony@gmail.com>
2024-05-05 01:43:28 +0300

    nodejs. cleanup the server

--- javascript/frameworks/nodejs/node2024/server2.js
+++ javascript/frameworks/nodejs/node2024/server2.js
@@ -50,27 +50,15 @@ const notFoundHandler = (req, res) => {
 const server = createServer((req, res) => {
   // wrapping in the callback and getting access to the req-res objects
   logger(req, res, () => {
-    if (req.url === '/api/users' && req.method === 'GET') {
-      res.setHeader('Content-Type', 'application/json');
-      res.write(JSON.stringify(users));
-      res.end();
-    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-      const id = req.url.split('/')[3];
-      const user = users.find((user) => user.id === parseInt(id));
-      res.setHeader('Content-Type', 'application/json');
-      if (user) {
-        res.write(JSON.stringify(user));
+    jsonMiddleware(req, res, () => {
+      if (req.url === '/api/users' && req.method === 'GET') {
+        getUsersHandler(req, res);
+      } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        getUserByIdHandler(req, res);
       } else {
-        res.statusCode = 404;
-        res.write(JSON.stringify({ message: "User not found!" }));
+        notFoundHandler(req, res);
       }
-      res.end();
-    } else {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 404;
-      res.write(JSON.stringify({ message: "Route not found!" }));
-      res.end();
-    }
+    })
   });
 });
 
