f6277e8e41913b3ec5c22d62f2966d8a06bf8cfd
Dmytro Onysko <dmytroony@gmail.com>
2024-05-06 01:53:28 +0300

    nodejs. working with fs(filesystem) module. reading file promise and async/await versions

--- javascript/frameworks/nodejs/node2024/fsDemo.js
+++ javascript/frameworks/nodejs/node2024/fsDemo.js
@@ -1,11 +1,28 @@
-import fs from 'fs'; // async/sync version
+// import fs from 'fs'; // async/sync version
+// 
+// // (async version: default): readFile() - callback, it's non-blocking
+// fs.readFile('./dataTxt.txt', 'utf8', (err, data) => { // (fileLocation, encodingMethod, callbackFn)
+//   if (err) throw err;
+//   console.log(data);
+// });
+// // (sync version): readFileSync() - it's blocking
+// const data = fs.readFileSync('./dataTxt.txt', 'utf8');
+// console.log(data);
 
-// (async version: default): readFile() - callback, it's non-blocking
-fs.readFile('./dataTxt.txt', 'utf8', (err, data) => { // (fileLocation, encodingMethod, callbackFn)
-  if (err) throw err;
-  console.log(data);
-});
+// (promise version): readFile() - Promise .then()
+import fs from 'fs/promises';
+// 
+fs.readFile('./dataTxt.txt', 'utf8')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
-// (sync version): readFileSync() - it's blocking
-const data = fs.readFileSync('./dataTxt.txt', 'utf8');
-console.log(data);
+// (async/await version): readFile()
+const readFile = async () => {
+  try {
+    const data = await fs.readFile('./dataTxt.txt', 'utf8');
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+readFile();
