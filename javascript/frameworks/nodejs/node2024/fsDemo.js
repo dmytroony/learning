8e0ff629052eb47e1c6797c51b5c008e8aaacfcc
Dmytro Onysko <dmytroony@gmail.com>
2024-05-06 02:17:44 +0300

    nodejs. working with fs module. writeFile and appendFile

--- javascript/frameworks/nodejs/node2024/fsDemo.js
+++ javascript/frameworks/nodejs/node2024/fsDemo.js
@@ -10,13 +10,15 @@
 // console.log(data);
 
 // (promise version): readFile() - Promise .then()
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
+//
+// fs.readFile('./dataTxt.txt', 'utf8')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 // 
-fs.readFile('./dataTxt.txt', 'utf8')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
 // (async/await version): readFile()
+// 
+import fs from 'fs/promises';
 const readFile = async () => {
   try {
     const data = await fs.readFile('./dataTxt.txt', 'utf8');
@@ -25,4 +27,26 @@ const readFile = async () => {
     console.error(error);
   }
 };
+
+const writeFile = async () => {
+  try {
+    await fs.writeFile('./dataTxt.txt', 'Hello from writeFile function');
+    console.log('File written to...');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// appendFile()
+const appendFile = async () => {
+  try {
+    await fs.appendFile('./dataTxt.txt', '\nHello from appendFile func');
+    console.log('Additional info appended to data file...');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+writeFile();
+appendFile();
 readFile();
