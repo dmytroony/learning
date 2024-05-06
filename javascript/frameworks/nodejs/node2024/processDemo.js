ba9442992aebbd0b8b5c037913bfff669695ce3b
Dmytro Onysko <dmytroony@gmail.com>
2024-05-06 16:13:47 +0300

    nodejs cc24 DONE. working with process module done

--- javascript/frameworks/nodejs/node2024/processDemo.js
+++ javascript/frameworks/nodejs/node2024/processDemo.js
@@ -1,1 +1,29 @@
-console.log(process);
+console.log(process.argv);
+// node processDemo.js import
+// node processDemo.js import -s
+
+console.log(process.argv[3]);
+
+console.log(process.env);
+console.log(process.env.LOGNAME);
+
+// pid
+console.log(process.pid); // nodejs process id
+
+// cwd() - Current Work Directory
+console.log(process.cwd());
+
+// title of process
+console.log(process.title);
+
+// memoryUsage()
+console.log(process.memoryUsage());
+
+// uptime() - uptime of process
+console.log(process.uptime());
+
+process.on('exit', code => console.log(`About to exit with code: ${code}`));
+
+// exit() - exit the process
+process.exit(0);
+console.log('Hello from after exit()');
