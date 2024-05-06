640bdc21908886528fcaea436c1ae94dc590e5d2
Dmytro Onysko <dmytroony@gmail.com>
2024-05-06 14:09:44 +0300

    nodejs. working with os(operating system) module

---
+++ javascript/frameworks/nodejs/node2024/osDemo.js
@@ -0,0 +1,12 @@
+import os from 'os';
+
+// userInfo()
+console.log(os.userInfo());
+console.log(os.userInfo().username);
+
+// totalmem(), freemem()
+console.log(os.totalmem());
+console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
