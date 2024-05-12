4c2cd3d83e5aaa1ecc71934ceedee2bc81bab762
Dmytro Onysko <dmytroony@gmail.com>
2024-05-13 00:14:00 +0300

    practicalTS. type guarding. challenge10 done

--- javascript/typescript/practical-ts/src/tutorial.ts
+++ javascript/typescript/practical-ts/src/tutorial.ts
@@ -698,4 +698,21 @@ type ValueType = string | number | boolean;
 let value:ValueType;
 const random = Math.random();
 value = random < .33 ? 'Hello' : random < .66 ? 123.456 : true;
-console.log(value);
+// console.log(value);
+
+function checkValue(value:ValueType):void {
+  if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+    return;
+  }
+  if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+    return;
+  }
+  console.log("boolean: ", value);
+}
+
+// checkValue('hello');
+// checkValue(356);
+// checkValue(true);
+checkValue(value);
