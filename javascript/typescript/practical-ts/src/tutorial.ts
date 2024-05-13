8fd999a8515c3ecdba7c887b3f64216fd83a81eb
Dmytro Onysko <dmytroony@gmail.com>
2024-05-13 06:45:13 +0300

    PracticalTS. challenge-11 done

--- javascript/typescript/practical-ts/src/tutorial.ts
+++ javascript/typescript/practical-ts/src/tutorial.ts
@@ -723,3 +723,9 @@
 type Dog = { type: 'dog'; name: string; bark: () => void };
 type Cat = { type: 'cat'; name: string; meow: () => void };
 type Animal = Dog | Cat;
+// 
+function makeSound(animal:Animal) {
+  if (animal.type === 'dog') animal.bark();
+  else animal.meow();
+}
+//
