ccf3f07685827b79f6e07d0341f9aca6f09db38f
Dmytro Onysko <dmytroony@gmail.com>
2024-10-02 01:34:22 +0300

    data structures and algorithms. array

--- ds-s_and_algs/dss.js
+++ ds-s_and_algs/dss.js
@@ -1,26 +1,89 @@
-let stack_1 = [];
+// * STACK - LIFO ds, stores objects into a sort of "vertical tower"
+// 
+// Usage cases:
+// - undo/redo in text editors
+// - back/forward through browser history
+// - backtracking algorithms (maze, file directories)
+// - calling functions
+// Methods: .push(), .pop()
 
-function stackIn(stack, el) {
-  return stack.push(el);
-}
+// let stack_1 = [];
 
-function stackOut(stack) {
-  return stack.pop();
-}
-stackIn(stack_1, "Minecraft");
-stackIn(stack_1, "Skyrim");
-stackIn(stack_1, "DOOM");
-stackIn(stack_1, "Borderlands");
-stackIn(stack_1, "FFVII");
+// function stackIn(stack, el) {
+//   return stack.push(el);
+// }
 
-console.log(stack_1);
+// function stackOut(stack) {
+//   return stack.pop();
+// }
+// stackIn(stack_1, "Minecraft");
+// stackIn(stack_1, "Skyrim");
+// stackIn(stack_1, "DOOM");
+// stackIn(stack_1, "Borderlands");
+// stackIn(stack_1, "FFVII");
 
+// console.log(stack_1);
 // stackOut(stack_1);
 // console.log(stack_1);
-
 // console.log(stack_1.shift());
 // console.log(stack_1);
+// console.log(stack_1.find(el => el === "FFVII"));
+// console.log(stack_1.find(el => el === "Fallout"));
 
-console.log(stack_1.find(el => el === "FFVII"));
-console.log(stack_1.find(el => el === "Fallout"));
- 
+
+// * QUEUE - FIFO ds,- a collection designed for holding elements 
+// prior to processing Linear data structure
+//
+// enqueue - push()
+// dequeue - shift()
+// class Queue {
+// 	constructor() {
+// 		this.items = {}
+// 		this.frontIndex = 0
+// 		this.backIndex = 0
+// 	}
+// 	enqueue(item) {
+// 		this.items[this.backIndex] = item
+// 		this.backIndex++
+// 		return item + ' inserted'
+// 	}
+// 	dequeue() {
+// 		const item = this.items[this.frontIndex]
+// 		delete this.items[this.frontIndex]
+// 		this.frontIndex++
+// 		return item
+// 	}
+// 	peek() {
+// 		return this.items[this.frontIndex]
+// 	}
+// 	get printQueue() {
+// 		return this.items;
+// 	}
+// }
+// const queue = new Queue();
+// console.log(queue.enqueue(7));
+// console.log(queue.enqueue(2));
+// console.log(queue.enqueue(6));
+// console.log(queue.enqueue(4));
+// let str1 = queue.printQueue;
+// console.log(str1);
+// console.log(queue.dequeue());
+// console.log(queue.peek());
+// let str2 = queue.printQueue;
+// console.log(str2);
+
+// vid2
+
+// ARRAY DS
+const students = ["Alex", "James", "Emma", "Louis", "Brian",];
+
+function findStudent(allStudents, studentName) {
+	for (let index = 0; index < allStudents.length; index++) {
+		if (allStudents[index] === studentName) {
+			console.log(`Found ${studentName}`);
+		}
+	}
+	console.log(allStudents.find(student => student === studentName));
+}
+
+findStudent(students, "Emma");
