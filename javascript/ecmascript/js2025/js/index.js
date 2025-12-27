c35604184b36a25cfb1fcf7fbe55d1219757c024
Dmytro Onysko <dmytroony@gmail.com>
2025-12-28 01:44:15 +0200

    latest

--- javascript/ecmascript/js2025/js/index.js
+++ javascript/ecmascript/js2025/js/index.js
@@ -1,4 +1,31 @@
 
-// JavaScript 2025 Crash Course
+// ------------- JavaScript 2025 Crash Course -------------
 
-document.body.style.background = "lightgreen"
+// document.body.style.background = "lightgreen";
+
+
+//// Implicit type coercion
+
+// eval()-------------
+
+console.log(eval("2 + 2"));
+// Expected output: 4
+
+console.log(eval(String("2 + 2")));
+// Expected output: 2 + 2
+
+console.log(eval("2 + 2") === eval(String("2 + 2")));
+
+console.log(eval(new String("2 + 2")));
+// Expected output: 2 + 2
+
+console.log(eval("2 + 2") === eval("4"));
+// Expected output: true
+
+console.log(eval("2 + 2") === eval(new String("2 + 2")));
+// Expected output: false
+
+console.log(eval("2 + 2") === eval(String("2 + 2")));
+// Expected output: true
+
+console.log(eval(new Number(456)))
